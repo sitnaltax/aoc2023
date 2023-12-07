cc8719c0231f3e9ce7522370f3df2ebad28dcaf2
Rob Herman <sitnaltax@gmail.com>
2023-12-07 10:17:57 -0500

    finish 5b

--- 5/5b.py
+++ 5/5b.py
@@ -1,12 +1,12 @@
-class Range:
-    def __init__(self, start, count):
-        self.start = start
-        self.count = count
-
 class MapEntry:
     def __init__(self, source, destination, count):
         self.source = source
         self.destination = destination
+        self.count = count
+
+class Range:
+    def __init__(self, source, count):
+        self.source = source
         self.count = count
 
 def read_next_map():
@@ -24,44 +24,66 @@ def read_next_map():
     map_to_return.sort(key=lambda entry: entry.source)
     return map_to_return
 
-# Given a range and a list of MapEntries, determines a list of output ranges
-def find_mapping(input_range, input_map):
-    output_ranges = []
-    relevant_entries = []
-    for entry in input_map:
-        if (entry.source + entry.count < input_range.start) and (entry.source <= input_range.start + input_range.count):
-            relevant_entries.append(entry)
-    # Now every entry in relevant_entries, plus every gap in the input range not covered by a mapping, needs an output range
-    #  
+# Given a number and a list of MapEntries, determines the output number
+def find_mapping(input_number, input_map):
     for possible_entry in input_map:
         difference = input_number - possible_entry.source
         if difference >= 0 and difference < possible_entry.count:
             return possible_entry.destination + difference
-    return output_ranges
+    return input_number
+
+# Given a number and a list of MapEntries, determines the possible input for which the given number was an output
+def find_reverse_mapping(output_number, input_map):
+    possible_inputs = []
+    for possible_entry in input_map:
+        difference = output_number - possible_entry.destination
+        if difference >= 0 and difference < possible_entry.count:
+            possible_inputs.append(possible_entry.source + difference)
+    if find_mapping(output_number, input_map) == output_number:
+        possible_inputs.append(output_number)
+    return possible_inputs
+    
 
 fstream = open("input.txt", 'r')
-seeds = []
+seed_ranges = []
 line = fstream.readline()
 (junk, payload) = line.split(':')
-for seed in payload.split():
-    seeds.append(int(seed))
-print(seeds)
+seeds_strings = payload.split()
+for i in range(0, len(seeds_strings), 2):
+    seed_ranges.append(Range(int(seeds_strings[i]), int(seeds_strings[i + 1])))
+
+def ranges_contain_value(target_num, ranges):
+    for possible_range in ranges:
+        difference = target_num - possible_range.source
+        if difference >= 0 and difference < possible_range.count:
+            print(target_num, possible_range.source, possible_range.count)
+            return True
+    return False
 
 maps = []
 
 for i in range(7):
     maps.append(read_next_map())
 
-values = []
-for seed in seeds:
-    value = seed
-    for map in maps:
-        value = find_mapping(value, map)
-    values.append(value)
+reversed_maps = list(reversed(maps))
 
-print(min(values))
+for possible_output in range(79400000, 999999999):
+    values_list = [possible_output]
+    for map in reversed_maps:
+        next_generation_values = []
+        for value in values_list:
+            next_generation_values += find_reverse_mapping(value, map)
+        values_list = next_generation_values
+    for value in values_list:
+        if ranges_contain_value(value, seed_ranges):
+            print(possible_output)
+            exit()
+    if possible_output % 100000 == 0:
+        print(possible_output)
 
-#lines = []
-#while line:
-#    lines.append(line)
-#    line = fstream.readline()
+# seeds = [3947715238]
+# for seed in seeds:
+#    value = seed
+#    for map in maps:
+#        value = find_mapping(value, map)
+#        print(value)
