bab67f92f402c19fce7f90818a1372d659830725
Rob Herman <sitnaltax@gmail.com>
2023-12-10 01:44:49 -0500

    remove day 9 clutter

--- 10/10a.py
+++ 10/10a.py
@@ -1,20 +1,3 @@
-class ReadingHistory:
-    def __init__(self, input_string):
-        self.initial_readings = [int(val) for val in input_string.split()]
-
-    def interpolate_next_value(self):
-        history_calculation_lines = [self.initial_readings]
-        while any(value != 0 for value in history_calculation_lines[-1]):
-            next_history_calculation_line = []
-            for index in range(len(history_calculation_lines[-1]) - 1):
-                next_history_calculation_line.append(history_calculation_lines[-1][index + 1] - history_calculation_lines[-1][index])
-            history_calculation_lines.append(next_history_calculation_line)
-        # OK, history_calculation_lines is now complete
-        history_calculation_lines[-1].append(0)
-        for line_index in reversed(range(len(history_calculation_lines) - 1)):
-            history_calculation_lines[line_index].append(history_calculation_lines[line_index][-1] + history_calculation_lines[line_index + 1][-1])
-        return history_calculation_lines [0][-1]
-
 # x is across, increasing to the right
 # y is down, increasing down
 class Coords:
