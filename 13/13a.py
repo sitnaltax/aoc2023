1b4c069044427318e3fea22504be666893aae93a
Rob Herman <sitnaltax@gmail.com>
2023-12-13 14:51:30 -0500

    fixed 13a

--- 13/13a.py
+++ 13/13a.py
@@ -22,12 +22,14 @@ class Puzzle:
         # now columns
         for column_index in range(len(self.lines[0]) - 1):
             summary_total = 0
-            for line in self.lines:
+            for (index, line) in enumerate(self.lines):
                 if len(line) == 1:
                     continue
                 if line[column_index] == "#":
                     summary_total += pow(2, index)
             self.column_summaries.append(summary_total)
+
+        print (self.column_summaries)
 
     def find_reflection(self):
         if len(self.lines) <= 1:
@@ -71,6 +73,7 @@ class Puzzle:
 
         if total_to_return == 0:
             print ("Couldn't find reflection")
+            return -1
         return total_to_return
 
 # don't forget off-by-one for the final metric
@@ -93,7 +96,8 @@ while len(puzzles[-1].lines) > 0:
     puzzles.append(read_puzzle(fstream))
 
 running_total = 0
-for puzzle in puzzles:
+for (index, puzzle) in enumerate(puzzles):
+    print(f"working on puzzle {index}")
     puzzle.calculate_summary_numbers()
     running_total += puzzle.find_reflection()
 
